7a409bb9aa08a1ef2ede6d932b7004287bc8f52d
sandeepmadmin <126542987+sandeepmadmin@users.noreply.github.com>
2023-03-15 11:05:26 +0530

    new change 3

--- file.sh
+++ file.sh
@@ -1,4 +1,4 @@
 echo "This is GITHUB and Jenkins Integration"
 echo "Looks Cool!"
 echo "test is done"
-echo "sandeep is training for scm job after webhook"
+echo "sandeep is training for scm job after webhook without push"
